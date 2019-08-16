b16d0cf3f7a0aaeb8898651e0de24d67984fe9ff
Marcin Marczak <marcin.marczak@pl.abb.com>
2019-08-16 15:11:39 +0200

    Updated DeliveryDto to include parcels

--- src/NPost.Services.Deliveries/Application/DTO/DeliveryDto.cs
+++ src/NPost.Services.Deliveries/Application/DTO/DeliveryDto.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NPost.Services.Deliveries.Core.Entities;
 
 namespace NPost.Services.Deliveries.Application.DTO
@@ -9,6 +11,7 @@ namespace NPost.Services.Deliveries.Application.DTO
         public string Status { get; set; }
         public string Notes { get; set; }
         public RouteDto Route { get; set; }
+        public IEnumerable<ParcelInDeliveryDto> Parcels { get; set; }
 
         public DeliveryDto()
         {
@@ -19,8 +22,10 @@ namespace NPost.Services.Deliveries.Application.DTO
             Id = delivery.Id;
             Status = delivery.Status.ToString();
             Notes = delivery.Notes;
+            Parcels = delivery.Parcels.Select(pid => new ParcelInDeliveryDto(pid));
             if (delivery.Route != null)
                 Route = new RouteDto(delivery.Route);
+
         }
     }
 }
