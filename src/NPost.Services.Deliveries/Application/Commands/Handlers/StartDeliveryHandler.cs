2386cde7a4f9c51d5aef6041d5c5992640b31328
Marcin Marczak <marcin.marczak@pl.abb.com>
2019-08-16 11:38:24 +0200

    StartDelivery command

---
+++ src/NPost.Services.Deliveries/Application/Commands/Handlers/StartDeliveryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Convey.CQRS.Commands;
+
+namespace NPost.Services.Deliveries.Application.Commands.Handlers
+{
+    public class StartDeliveryHandler : ICommandHandler<StartDelivery>
+    {
+        public async Task HandleAsync(StartDelivery command)
+        {
+
+        }
+    }
+}
