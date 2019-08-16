5dab0dee65ea7c7fe9b4c1248caf05fb33cf2b7e
Marcin Marczak <marcin.marczak@pl.abb.com>
2019-08-16 14:55:05 +0200

    Save parcel in ParcelAddedHandler

--- src/NPost.Services.Deliveries/Application/Events/Handlers/ParcelAddedHandler.cs
+++ src/NPost.Services.Deliveries/Application/Events/Handlers/ParcelAddedHandler.cs
@@ -2,22 +2,26 @@
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
 using Microsoft.Extensions.Logging;
+using NPost.Services.Deliveries.Core.Entities;
+using NPost.Services.Deliveries.Core.Repositories;
 
 namespace NPost.Services.Deliveries.Application.Events.Handlers
 {
     public class ParcelAddedHandler : IEventHandler<ParcelAdded>
     {
         private readonly ILogger<ParcelAddedHandler> _logger;
+        private readonly IParcelsRepository _parcelsRepository;
 
-        public ParcelAddedHandler(ILogger<ParcelAddedHandler> logger)
+        public ParcelAddedHandler(ILogger<ParcelAddedHandler> logger, IParcelsRepository parcelsRepository)
         {
             _logger = logger;
+            _parcelsRepository = parcelsRepository;
         }
 
         public async Task HandleAsync(ParcelAdded parceAddedEvent)
         {
             _logger.LogInformation($"Parcel added event: {parceAddedEvent.ParcelId}");
-            return;
+            await _parcelsRepository.AddAsync(new Parcel(parceAddedEvent.ParcelId, string.Empty));
         }
     }
 }
