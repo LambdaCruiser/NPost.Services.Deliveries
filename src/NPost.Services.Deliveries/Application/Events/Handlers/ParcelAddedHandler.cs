c134b5e44a3603472ea548360973b69508c5f0f2
Marcin Marczak <marcin.marczak@pl.abb.com>
2019-08-16 16:00:06 +0200

    Loading parcel Address by direct API call

--- src/NPost.Services.Deliveries/Application/Events/Handlers/ParcelAddedHandler.cs
+++ src/NPost.Services.Deliveries/Application/Events/Handlers/ParcelAddedHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
+using Convey.HTTP;
 using Microsoft.Extensions.Logging;
+using NPost.Services.Deliveries.Application.DTO;
 using NPost.Services.Deliveries.Core.Entities;
 using NPost.Services.Deliveries.Core.Repositories;
 
@@ -11,17 +13,29 @@ namespace NPost.Services.Deliveries.Application.Events.Handlers
     {
         private readonly ILogger<ParcelAddedHandler> _logger;
         private readonly IParcelsRepository _parcelsRepository;
+        private readonly IHttpClient _client;
 
-        public ParcelAddedHandler(ILogger<ParcelAddedHandler> logger, IParcelsRepository parcelsRepository)
+        public ParcelAddedHandler(ILogger<ParcelAddedHandler> logger, IParcelsRepository parcelsRepository, IHttpClient client)
         {
             _logger = logger;
             _parcelsRepository = parcelsRepository;
+            _client = client;
         }
 
         public async Task HandleAsync(ParcelAdded parcelAddedEvent)
         {
+            //TODO - nie chcemy hardkodowanej konfiguracji, chcemy DNSa
+            var parcelDto = await _client.GetAsync<ParcelDto>($"http://localhost:5002/parcels/{parcelAddedEvent.ParcelId}");
+            if (parcelDto is null)
+            {
+                throw new ArgumentException("Parcel not fonud");
+            }
+
+            var parcel = new Parcel(parcelAddedEvent.ParcelId, parcelDto.Address);
+
             _logger.LogInformation($"Parcel added event: {parcelAddedEvent.ParcelId}");
-            await _parcelsRepository.AddAsync(new Parcel(parcelAddedEvent.ParcelId, string.Empty));
+
+            await _parcelsRepository.AddAsync(parcel);
         }
     }
 }
