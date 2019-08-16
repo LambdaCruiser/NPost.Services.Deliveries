9b1ff0014e841bc4b08a0fe81a106833fe6afe38
Marcin Marczak <marcin.marczak@pl.abb.com>
2019-08-16 11:47:20 +0200

    Added logging to StartDeliveryHandler

--- src/NPost.Services.Deliveries/Application/Commands/Handlers/StartDeliveryHandler.cs
+++ src/NPost.Services.Deliveries/Application/Commands/Handlers/StartDeliveryHandler.cs
@@ -1,13 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Convey.CQRS.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace NPost.Services.Deliveries.Application.Commands.Handlers
 {
     public class StartDeliveryHandler : ICommandHandler<StartDelivery>
     {
-        public async Task HandleAsync(StartDelivery command)
-        {
+        private readonly ILogger<StartDeliveryHandler> _logger;
 
+        public StartDeliveryHandler(ILogger<StartDeliveryHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleAsync(StartDelivery command)
+        {
+            _logger.LogInformation($"Starting a delivery {command.DeliveryId}");
+            return Task.CompletedTask;
         }
     }
 }
